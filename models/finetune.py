a537825717c71fa5bf5bcc935bfd0851eb05c781
Elin van Marle <elinvanmarle@gmail.com>
2026-04-02 19:03:07 +0200

    worked on finetuning and the data tokenizer

--- models/finetune.py
+++ models/finetune.py
@@ -3,13 +3,12 @@ from transformers import AutoModelForSequenceClassification
 import torch
 from transformers import AutoTokenizer
 from transformers import Trainer, TrainingArguments
-from data import preprocess_data
-from datasets import Dataset
+from data import build_tokenizer
 
 
 def fine_tune(train_dataset, test_ds, seed):
     """
-    _summary_
+    fine tuning of a pretrained model
 
     Args:
         train_dataset (_type_): _description_
@@ -21,12 +20,12 @@ def fine_tune(train_dataset, test_ds, seed):
     device = 0 if torch.cuda.is_available() else -1
 
     #Preprocess data
-    X_train, y_train = preprocess_data(train_dataset)
-    X_test, y_test = preprocess_data(test_ds)
-
-
-    #Load model
-    model_name = "distilbert-base-uncased"
+    model_name = "distilbert-base-uncased" #We are using distilBERT
+    tokenize_fn = build_tokenizer(model_name)
+    tokenized_train = train_dataset.map(tokenize_fn, batched=True)
+    tokenized_test = test_ds.map(tokenize_fn)
+    
+    #Load a pretrained model
     model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2).to(
         device
     )
@@ -42,9 +41,9 @@ def fine_tune(train_dataset, test_ds, seed):
         weight_decay=0.01,
     )
 
-    # trainer = Trainer(
-    #     model=model,
-    #     args=training_args,
-    #     train_dataset=tokenized_train,
-    #     eval_dataset=tokenized_test,
-    # )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_test,
+    )
